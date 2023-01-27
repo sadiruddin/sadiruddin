7e404cfe04364af626797ce7303b8630dcc794b7
Sadir <sadiruddin@gmail.com>
2023-01-27 09:44:21 +0530

    Added test cases

--- src/test/java/tcom/sadir/test/SpeakingClockTest.java
+++ src/test/java/tcom/sadir/test/SpeakingClockTest.java
@@ -26,4 +26,25 @@ public class SpeakingClockTest {
         String actual = clock.speakTimeInWords("12:00");
         Assert.assertEquals("It's midday", actual);
     }
+
+    @Test
+    public void testEightThirty() {
+        String actual = clock.speakTimeInWords("08:30");
+        Assert.assertEquals("It's eight thirty", actual);
+    }
+
+    @Test
+    public void testNineThirty() {
+        String actual = clock.speakTimeInWords("21:09");
+        Assert.assertEquals("It's nine nine", actual);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidTime1() {
+       clock.speakTimeInWords("25:30");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidTime2() {
+        clock.speakTimeInWords("AA:30");
+    }
 }
